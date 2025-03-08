b13254c233555aaef0c5a73bd3ff407d52793ffa
verah mokaya <verahmokaya475@gmail.com>
2025-03-08 18:30:15 +0300

    Flowcontrol lob done

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+	if (distanceInFeet > 2500) {
+		return `No can do.`;
+	}
+	else if (distanceInFeet > 2000) {
+		return `I will gladly take your thirty bucks.`;
+	}
+	else if (distanceInFeet <= 400) {
+		return `This one is on me!`;
+	}
+  else if (distanceInFeet > 400 < 2000) {
+		return `That will be twenty bucks.`;}
+
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC' ?  "Ok, sounds good." : "No go."
+  // if(city === 'NYC'){
+  //   return "Ok, sounds good."
+  // }
+  // else {
+  //   return "No go."
+  // }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if(tip === 'generous'){
+    return  "Thank you so much."
+  }
+  else if (tip === 'not as generous'){
+    return "Thank you."
+  }
+  else {
+    return 'Bye.'
+  }
+  
 }
